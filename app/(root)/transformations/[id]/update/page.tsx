d131f4743e7b5b8509af5cb325e111dbaf6b7f34
Ahmed MELLIT <formilaire2019@gmail.com>
2024-02-25 10:31:34 +0100

    first commit

---
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Update transformation page
+    </div>
+  )
+}
+
+export default page
---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Add transformation type page
+    </div>
+  )
+}
+
+export default page
